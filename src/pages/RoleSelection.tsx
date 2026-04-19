93285f0d7a6744e719d3b6067d1282fa223e303d
Gideon Bamuleseyo <gideon.bamuleseyo@toptal.com>
2026-04-19 18:15:08 -0500

    Add role selection

---
+++ src/pages/RoleSelection.tsx
@@ -0,0 +1,10 @@
+export default function RoleSelection() {
+  return (
+    <div>
+      <h2>How do you wanna play?</h2>
+      <p>This sets your role in the game.</p>
+      <button>Chad</button>
+      <button>Vixen</button>
+    </div>
+  );
+}
